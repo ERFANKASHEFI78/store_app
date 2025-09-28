fb062abff3323f3e9adc683144938c5da84a6c29
Erfan Kashfi <erfankashefi999 (you@example.com)@gmail.com>
2025-09-28 15:17:52 +0330

    App Store was created!

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:store_app/ForgotPasswordPage.dart';
+import 'package:store_app/home_page.dart';
+import 'package:store_app/item_model.dart';
+import 'package:store_app/signup_page.dart';
+import 'package:store_app/login_page.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  
+  await Hive.initFlutter();
+  Hive.registerAdapter(ItemAdapter());
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+     initialRoute: '/login',
+      routes: {
+    '/login': (context) => LoginPage(),
+    '/signup': (context) => SignUpPage(),
+    '/home': (context) => HomePage(),
+    '/forgotPassword' :(context)=>ForgotPasswordPage()
+        },
+      debugShowCheckedModeBanner: false,
+      title: 'Store App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+     
+    );
+  }
+}
